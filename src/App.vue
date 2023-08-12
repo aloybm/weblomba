61cd8add11723e3bab2022dea15f6635a5820880
aloybm <manullangaloysius@gmail.com>
2023-08-12 18:12:24 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -3,54 +3,60 @@
     <NavBar />
     <main>
       <div class="flex">
-        <section
-          id="section"
-          class="relative mb-8 mt-20 ml-20  xl:left-[800px] xl:top-60 "
-        >
-          <h1
-            class=" font-normal font-beranda w-auto xl:w-[410px] h-auto xl:h-[120px]  text-black text-5xl g:text-6xl  leading-[60px]"
-          >
-            Lorem Ipsum dolor Sit amet
-          </h1>
-          <p
-            class="text-black text-lg md:text-xl mt-4 md:mt-6 text-justify w-auto xl:w-[477px] font-medium"
-          >
-            Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa
-            mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.
-          </p>
-          <button
-            class="bg-[#EED4B5] hover:bg-orange-200 text-white py-2 px-4 rounded-full text-xl mt-12 xl:w-[254px] xl:h-[64px] xl:text-[36px]"
-          >
-            Daftar
-          </button>
+        <section id="beranda" class="relative mb-8 mt-20 ml-20 xl:left-[800px] xl:top-60">
+          <h1 class=" font-normal font-beranda w-auto xl:w-[410px] h-auto xl:h-[120px] text-black text-5xl g:text-6xl leading-[60px]">Lorem Ipsum dolor Sit amet</h1>
+		      <p class="text-black text-lg md:text-xl mt-4 md:mt-6 text-justify w-auto xl:w-[477px] font-medium">
+			      Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.
+		      </p>
+		      <button class="bg-[#EED4B5] hover:bg-orange-200 text-white py-2 px-4 rounded-full text-xl mt-12 xl:w-[254px] xl:h-[64px] xl:text-[36px]">Daftar</button>
         </section>
       </div>
       <div class="flex">
-      <section id="apa" class="relative mb-8  ml-5 xl:top-96">
-        <div class="flex">
-        <h1 class="vertical-text font-bold w-2 text-5xl xl:text-[108px] leading-[103px]">APA</h1>
-        <h1 class="vertical-text font-bold w-2 text-5xl xl:text-[108px] leading-[103px] mt-40 ">ITU ?</h1>
-          <div class="ml-32 mt-10 p-10">
-          <h1
-            class=" relative font-normal font-b w-auto xl:w-[353px] h-auto xl:h-[168px]  text-black text-5xl leading-[60px] g:text-6xl "
-          >
-            Lorem Ipsum dolor Sit amet
-          </h1>
-          <p
-            class="text-black text-lg md:text-xl mt-4 md:mt-6 text-justify w-auto xl:w-[317px] xl:h-[229px] font-medium"
-          >
-            Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa
-            mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.
-          </p>
-          </div>
-          <div class="flex">
-            <p class="font-number font-normal text-5xl text-[#EED4B5]">1</p>
-          </div>
-        </div>
-        <div>
+        <section id="pendaftaran" class="relative mb-8 ml-5 xl:top-96">
+            <div class="flex">
+              <div class="flex flex-row p-12 pl-5">
+			<h1 class="vertical-text font-bold leading-[103px] ">APA</h1>
+			<h1 class="vertical-text font-bold mt-28 leading-[103px] pl-20 ">ITU ?</h1>
+              </div>
+			<div class="ml-32 mt-10 p-10 pl-0">
+				<h1 class=" relative font-normal font-b w-auto xl:w-[353px] h-auto xl:h-[168px] text-black text-5xl leading-[60px] g:text-6xl ">Lorem Ipsum dolor Sit amet</h1>
+				<p class="text-black text-lg md:text-xl mt-4 md:mt-6 text-justify w-auto xl:w-[317px] xl:h-[229px] font-medium">
+					 Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.
+				</p>
+			</div>
+			<div class="flex flex-col pl-7">
+				<div class="flex">
+					<p class="font-number font-normal text-6xl xl:ml-20 xl:mt-20 text-[#EED4B5]">1</p>
+					<div class="flex container pl-14 mt-10 p-10 text-2xl leading-[36px] xl:w-[470px] xl:pr-0 mr-0 text-justify">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipiscing
+						</p>
+					</div>
+				</div>
+				<div class="flex">
+					<p class="font-number font-normal text-6xl xl:ml-20 xl:mt-10 text-[#EED4B5]">2</p>
+					<div class="flex container pl-12 p-10 text-2xl leading-[36px] xl:w-[470px] xl:pr-0 mr-0 text-justify">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipiscing
+						</p>
+					</div>
+				</div>
+				<div class="flex ">
+					<p class="font-number font-normal text-6xl xl:ml-20 xl:mt-10 text-[#EED4B5]">3</p>
+					<div class="flex container pl-12 p-10 text-2xl leading-[36px] xl:w-[470px] xl:pr-0 mr-0 text-justify">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipiscing
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+        </section>
+      </div>
+      <div class="flex">
+        <section id="pembekalan" class="relative">
           
-        </div>
-      </section>
+        </section>
       </div>
     </main>
   </div>
@@ -58,10 +64,12 @@
 
 <script>
 import NavBar from "./components/NavBar.vue";
+//import Pendaftaran from './components/pendaftaran.vue';
 
 export default {
   components: {
     NavBar,
+    //Pendaftaran
   },
 };
 </script>
@@ -74,18 +82,17 @@ body {
   font-family: 'Baloo Bhai';
 }
 .vertical-text {
-           margin-left: 100px;
-           text-align: center;
-           width: 0px;
-           word-break: break-all;
-           white-space: pre-wrap;
-           /* color: (248, 186, 113, 0.55) */
-           color : #F8BA71
+    width: 50px;
+    font-weight: bold;
+    font-size: 108px;
+    word-wrap: break-word;
+    color: #F8BA71;
 }
-.font-b{
+
+.font-b {
   font-family: 'Baloo';
 }
-.font-number{
+.font-number {
   font-family: 'Titan One';
 }
 </style>
---
+++ src/components/pendaftaran.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="p-6 pr-20 mx-auto pt-[70px] ">
+    <nav>
+      <div class="container flex justify-end text-lg">
+        <a href="#beranda" class="text-black font-medium cursor-pointer border-b-4 border-orange-100 mx-1.5 sm:mx-6">Beranda</a>
+        <a href="#pendaftaran" class="border-b-4 border-transparent text-gray-300 hover:text-black hover:font-mediun cursor-pointer  hover:border-orange-100 mx-1.5 sm:mx-6">Pendaftaran</a>
+        <a href="#peraturan" class="border-b-4 border-transparent text-gray-300 hover:text-black font-medium cursor-pointer  hover:border-orange-100 mx-1.5 sm:mx-6">Peraturan</a>
+        <a href="#klasemen" class="border-b-4 border-transparent text-gray-300 hover:text-black font-medium cursor-pointer  hover:border-orange-100 mx-1.5 sm:mx-6">Klasemen</a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Pendaftaran",
+};
+</script>
