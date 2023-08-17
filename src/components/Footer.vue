e2d4735ca2f099160e8a17ad1193fc43bbdd11c7
aloybm <manullangaloysius@gmail.com>
2023-08-17 22:01:16 +0700

    done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,10 @@
                 <div class="h-2 justify-end w-1/2 bg-gradient-to-l from-transparent to-[#EED4B5]"></div>
             </div>
             <ul>
-                <li><p> <a href="http://phonic.ai"> phonic.ai </a> </p></li>
+                <li><a href="https://survey.phonic.ai/64d2501d7ae5d13742da44c1">
+                    <p> phonic.ai</p>
+                    </a>
+                </li>
                 <li><p></p></li>
                 <li><p></p></li>
             </ul>
