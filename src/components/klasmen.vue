7aa7a14d208d288c13f50af5b6fb28965b54e744
aloybm <manullangaloysius@gmail.com>
2023-08-17 20:58:03 +0700

    klasemen

--- src/components/klasmen.vue
+++ src/components/klasmen.vue
@@ -8,10 +8,10 @@
         <!-- <div class="lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0 m-20 items-center justify-center">
           <img src="../assets/klasmen.png" class="self-center left-20 item h-full w-full" alt="">
         </div> -->
-        <img src="../assets/klasmen.png" alt="">
+        <img src="../assets/klasmen.png"  class="w-11/12" alt="">
         <div class="lg:flex-row lg:flex flex-col leading-none lg:text-8xl text-7xl hidden">
           <h1 class="vertical-text font-bold">KLAS</h1>
-          <h1 class="vertical-text font-bold mt-20 xl:mt-40 pl-auto xl:pl-20">MEN</h1>
+          <h1 class="vertical-text font-bold mt-20 xl:mt-40 pl-auto xl:pl-10">MEN</h1>
         </div>
       </div>
 
